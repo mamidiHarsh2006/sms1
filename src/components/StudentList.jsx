4dc10cec831071744423100a142d02334a49f2ed
mamidiHarsh2006 <mamidiharsh2006@gmail.com>
2025-04-28 08:43:16 +0530

    Update StudentList.jsx

--- src/components/StudentList.jsx
+++ src/components/StudentList.jsx
@@ -6,12 +6,12 @@ function StudentList() {
   const [students, setStudents] = useState([]);
 
   const fetchStudents = async () => {
-    const res = await axios.get('http://localhost:5000/students');
+    const res = await axios.get('https://sms-uagq.onrender.com/students');
     setStudents(res.data);
   };
 
   const deleteStudent = async (id) => {
-    await axios.delete(`http://localhost:5000/students/${id}`);
+    await axios.delete(`https://sms-uagq.onrender.com/students/${id}`);
     fetchStudents();
   };
 
